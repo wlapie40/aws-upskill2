dfcdf96361684e33bd378927da538aefc00df1b5
Szymon Figiel (PGS Software) <sfigiel@pgs-soft.com>
2019-10-11 12:19:10 +0200

    hotfix 1.0.01

--- app.py
+++ app.py
@@ -178,4 +178,4 @@ api.add_resource(DownloadS3BucketFile,
                  '/download/api/v1.0/file')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
